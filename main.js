980133a0f73f2c9c9ee55ac30ded99b2e34fa27a
JulieNonnon <julie.nonnon@gmail.com>
2024-08-21 22:09:33 +0200

    cube created + animation

--- main.js
+++ main.js
@@ -11,3 +11,37 @@ const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.inner
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+// CREATE A CUBE
+
+const textureLoader = new THREE.TextureLoader();
+const texture = textureLoader.load('/assets/images/tntcrate.gif')
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+
+//const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = [
+    new THREE.MeshBasicMaterial({ map: texture }),
+    new THREE.MeshBasicMaterial({ map: texture }),
+    new THREE.MeshBasicMaterial({ map: texture }), // top of cube
+    new THREE.MeshBasicMaterial({ map: texture }), // bottom of cube
+    new THREE.MeshBasicMaterial({ map: texture }),
+    new THREE.MeshBasicMaterial({ map: texture })
+];
+
+const cube = new THREE.Mesh( geometry, material );
+
+scene.add(cube);
+
+camera.position.z = 5;
+
+// RENDERING THE SCENE
+
+function animate() {
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render( scene, camera );
+}
+renderer.setAnimationLoop( animate );
+
