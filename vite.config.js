a5a27452da09353a9b977d98435f85c57a4926f5
JulieNonnon <julie.nonnon@gmail.com>
2024-09-18 22:50:55 +0200

    created vite.config.js to deploy with Vite

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Floating_TNT_Crate_ThreeJS/',  // Remplacez par le nom de votre dépôt GitHub
+  build: {
+    outDir: 'dist',  // Dossier où le build sera généré
+  },
+});
