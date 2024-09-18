bf2ccaf76d872eb41454e019be28cf1e2863e3b0
JulieNonnon <julie.nonnon@gmail.com>
2024-09-18 23:22:03 +0200

    Tried to fix Uncaught TypeError

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/Floating_TNT_Crate_ThreeJS/',  // Nom du dépôt GitHub
-  build: {
-    outDir: 'dist',  // Dossier où le build sera généré
-  },
+//   build: {
+//     outDir: 'dist',  // Dossier où le build sera généré
+//   },
 });
