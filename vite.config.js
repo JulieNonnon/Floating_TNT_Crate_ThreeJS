07deb25bc7a0986a1ce7da21776a4be887f8cbe4
JulieNonnon <julie.nonnon@gmail.com>
2025-04-25 00:52:08 +0200

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@
  */
 
 export default {
-    base: process.env.NODE_ENV === 'production' ? '/Floating_TNT_Crate_ThreeJS/' : ''
+    base: process.env.NODE_ENV === 'production' ? '/ Floating_TNT_Crate_ThreeJS/' : ''
   };
