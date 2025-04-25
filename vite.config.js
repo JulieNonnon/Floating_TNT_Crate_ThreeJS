455ce63cd1e30d6c77ae0b638db1f558a0511053
JulieNonnon <julie.nonnon@gmail.com>
2025-04-25 23:13:40 +0200

    updated vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-/**
- * @type {import('vite').UserConfig}
- */
+// /**
+//  * @type {import('vite').UserConfig}
+//  */
 
-export default {
-    base: process.env.NODE_ENV === 'production' ? '/ Floating_TNT_Crate_ThreeJS/' : ''
-  };
+// export default {
+//     base: process.env.NODE_ENV === 'production' ? '/ Floating_TNT_Crate_ThreeJS/' : ''
+//   };
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/Floating_TNT_Crate_ThreeJS/', // <-- repo GitHub
+});
